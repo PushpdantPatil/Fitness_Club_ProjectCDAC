7d63f1bde8b6a353b7a58f78d3158f9153d999fc
Pushpdant <pushpdantpatil14@gmail.com>
2023-02-27 23:34:28 +0530

    added report controller

--- Backend/FitnessClub/src/main/java/com/app/pojos/Report.java
+++ Backend/FitnessClub/src/main/java/com/app/pojos/Report.java
@@ -2,7 +2,11 @@ package com.app.pojos;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -17,19 +21,39 @@ public class Report extends BaseEntity {
 	@Column(length=1000)
 	private String diet;
 
+	@JsonIgnore
 	@OneToOne
+	@JoinColumn(name = "member_id")
 	private Member member;
+	
+	
+
+	public Report() {
+		super();
+	}
+
+
 
 	public Report(double height, double weight, String workout, String diet, Member member) {
 		super();
-		this.height = height;
-		this.weight = weight;
+	this.height = height;
+	this.weight = weight;
 		this.workout = workout;
 		this.diet = diet;
 		this.member = member;
 	}
 	
 	
+
+	public Report(double height, double weight, String workout, String diet) {
+		super();
+		this.height = height;
+		this.weight = weight;
+		this.workout = workout;
+		this.diet = diet;
+	}
+
+
 
 	public Report(String workout, String diet) {
 		super();
